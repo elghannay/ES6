2e37e5b9e04f92e551061802c022cf28ffcef0f7
elghannay <mohamedelghannay@gmail.com>
2020-03-28 00:59:20 +0100

    objectliterals

---
+++ objectLiterals.js
@@ -0,0 +1,35 @@
+// if an object has a property key is the same as the value you can omit the latter
+
+function createBookShop(inventory) {
+    return {
+        inventory: inventory,
+        inventoryValue: function () {
+            return inventory.reduce((sum, book) => sum + book.price, 0);
+        },
+        priceForTitle: function (title) {
+            return inventory.filter(book => book.title === title)
+        }
+    }
+}
+const inventory = [
+    { title: 'harry Potter', price: 30 },
+    { title: 'eloquent js', price: 22.3 },
+];
+
+const bookStore = createBookShop(inventory);
+bookStore.inventoryValue();
+bookStore.priceForTitle('eloquent js');
+
+// a refactored version of the function 
+
+function createBookShop(inventory) {
+    return {
+        inventory,
+        inventoryValue() {
+            return inventory.reduce((sum, book) => sum + book.price, 0);
+        },
+        priceForTitle(title) {
+            return inventory.filter(book => book.title === title)
+        }
+    }
+}
