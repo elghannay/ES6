a8d11785db03664db159cf3f7b171d2af2cb3d64
elghannay <mohamedelghannay@gmail.com>
2020-03-30 19:54:47 +0100

    rest and distructuring

---
+++ distructuring.js
@@ -0,0 +1,73 @@
+/******************* destructuring : can be used to access properties of objects *********************/
+
+const car = { type: 'Mercedez', price: 45 };
+
+const { type } = car;
+const { price } = car;
+
+/******************* destructuring : can be used to pass arguments to functions *********************/
+
+let savedFile = { name: 'pdf', extension: 'jpg', size: 24433 }
+function print({ name, extension, size }) {
+    console.log(`the ${name} has an extension of ${extension} with a size of ${size}`);
+}
+print(savedFile)
+
+/******************* destructuring : can be used to access elements of arrays *********************/
+
+let companies = ['google', 'facebook', 'instagram'];
+let [companyOne, ...names] = companies; // instead of companyOne = companies[0] // notice the brackets
+console.log(companyOne);
+console.log(names);
+
+/******************* destructuring : can be used to access array of objects *********************/
+
+let networks = [
+    { name: 'google', location: 'mountain View' },
+    { name: 'facebook', location: 'London' },
+    { name: 'Instagram ', location: 'San Francisco' }
+];
+
+let [{ name, location }] = networks;
+console.log(name);
+console.log(location);
+
+let firstRow = networks[1].name;
+console.log(firstRow);
+
+let google = { name: 'google', locations: ['tetouan', 'tanger', 'Safi'] }
+let { locations: [locationOne, locationTwo, locationTree] } = google;
+console.log(locationOne);
+console.log(locationTwo);
+console.log(locationTree);
+
+/******************* destructuring : don't have to use the exact order of parameters *********************/
+
+let user = {
+    username: 'elghannay',
+    password: 'password',
+    dayOfbirth: 34355,
+    mood: '🤣',
+    city: 'tetouan'
+}
+function signUp({ username, password, dayOfbirth, mood, city }) {
+    // logic goes here
+}
+
+/******************* destructuring : convert an array of arrays to array of objects *********************/
+
+let points = [
+    [4, 5],
+    [5, 8],
+    [8, 90],
+    [34, 75]
+];
+
+let pointsObj = points.map(point => {
+    return { x: point[0], y: point[1] };
+})
+console.log(pointsObj);
+
+let pointsObjEnhanced = points.map(([x, y]) => { return { x: x, y: y } }); // is the same as { x, y }
+console.log(pointsObjEnhanced);
+
--- rest&spead.js
+++ rest&spead.js
@@ -3,14 +3,31 @@
 
 function add(a, b, c, d, e, f, d) {
     let numbers = [a, b, c, d, e, f, d];
-    return numbers.reduce((total, number) => total + number, 0)
+    return numbers.reduce((sum, number) => sum + number, 0);
 }
 
 console.log(add(1, 2, 3, 4, 5, 6, 7));
 
 function addTwo(...numbers) {
-    return numbers.reduce((sum, number) => sum + number, 0)
+    return numbers.reduce((sum, number) => sum + number, 0);
 }
 console.log(addTwo(1, 2, 3, 4, 5, 6, 7));
+
 /*********************** the spread operator ******************** */
 // get whatever in the array and spread it.
+
+const paletteOne = ['ýellow', 'red'];
+const paletteTwo = ['brown', 'blue'];
+
+const palette = [...paletteOne, ...paletteTwo];
+console.log(palette);
+
+function grocery(...items) {
+    if (items.indexOf('milk') < 0)
+        return ['milk', ...items];
+    return [...items];
+}
+
+console.log(grocery('fruits','peach'));
+
+
