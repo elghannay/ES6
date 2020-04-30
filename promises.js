622a05ddfb0694cd9df1d4edb7118228f9bc0497
elghannay <mohamedelghannay@gmail.com>
2020-04-30 22:42:02 +0000

    promises

--- promises.js
+++ promises.js
@@ -35,5 +35,91 @@ fetch(url)
 // from the server more precisely code 404, so the only way we get that code is when we fail to reach the server
 
 
+const btn = document.querySelector('button');
+
+//This function moves an element "amount" number of pixels after a delay.
+//If the element will stay on screen, we move the element and call the onSuccess callback function
+//If the element will move off screen, we do not move the element and instead call the onFailure callback
+const moveX = (element, amount, delay, onSuccess, onFailure) => {
+    setTimeout(() => {
+        const bodyBoundary = document.body.clientWidth;
+        const elRight = element.getBoundingClientRect().right;
+        console.log('elright', elRight);
+        const currLeft = element.getBoundingClientRect().left;
+        console.log('elLeft', currLeft);
+        if (elRight + amount > bodyBoundary) {
+            onFailure();
+        }
+        else {
+            console.log(element.style.transform);
+            element.style.transform = `translateX(${currLeft + amount}px)`;
+            // we added the current left so the button element can change it inline style  
+            onSuccess();
+        }
+    }, delay);
+};
+
+// LOOK AT THIS UGLY MESS!
+moveX(
+    btn,
+    300,
+    1000,
+    () => {
+        //success callback
+        moveX(
+            btn,
+            300,
+            1000,
+            () => {
+                //success callback
+                moveX(
+                    btn,
+                    300,
+                    1000,
+                    () => {
+                        //success callback
+                        moveX(
+                            btn,
+                            300,
+                            1000,
+                            () => {
+                                //success callback
+                                moveX(
+                                    btn,
+                                    300,
+                                    1000,
+                                    () => {
+                                        //success callback
+                                        alert('YOU HAVE A WIDE SCREEN!');
+                                    },
+                                    () => {
+                                        //failure callback
+                                        alert('CANNOT MOVE FURTHER!');
+                                    }
+                                );
+                            },
+                            () => {
+                                //failure callback
+                                alert('CANNOT MOVE FURTHER!');
+                            }
+                        );
+                    },
+                    () => {
+                        //failure callback
+                        alert('CANNOT MOVE FURTHER!');
+                    }
+                );
+            },
+            () => {
+                //failure callback
+                alert('CANNOT MOVE FURTHER!');
+            }
+        );
+    },
+    () => {
+        //failure callback
+        alert('CANNOT MOVE FURTHER!');
+    }
+);
 
 
