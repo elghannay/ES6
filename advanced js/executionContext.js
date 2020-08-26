0fd156c582f5dc4f98c75977087b62e477be24a8
elghannay <mohamedelghannay@gmail.com>
2020-08-26 06:38:03 +0100

    more about the wierd parts

--- advanced js/executionContext.js
+++ advanced js/executionContext.js
@@ -1,6 +1,7 @@
 // window;
 // window === this;
 // hoisting example
+
 var a = 'banana';
 function run() {
   console.log(a);
@@ -11,8 +12,42 @@ run();
 
 // that's why we need to avoid using VAR and stick with let and const.
 // function declaration get hoisted and function expressions don't
+
 function mary(person1, person2) {
   console.log(arguments);
   return true;
 }
 console.log(mary('ahmed', 'youssef'));
+
+// first bigBrother will be added a containing execution context.
+// then the two littleBrother functions are registered at the creation phase.
+// and overwrite each other, since they have the same name.
+// once we stop on the return statement we call only the last one.
+function bigBrother() {
+  function littleBrother() {
+    return 'it is me!';
+  }
+  return littleBrother();
+  function littleBrother() {
+    return 'no me!';
+  }
+}
+// variable envirenment;
+
+function two() {
+  var isValid;
+}
+function one() {
+  var isValid = false;
+  two();
+}
+var isValid = 'banana';
+one();
+console.log(isValid);
+
+// confusing parts about JS
+const hey = function doodle() {
+  return 'hey';
+};
+hey();
+doodle();
--- this inside a class.js
+++ this inside a class.js
@@ -1,25 +1,25 @@
 class Timer {
-    constructor(startButton, pauseButton, duration) {
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-        this.duration = duration;
-        this.startButton.addEventListener('click', this.start);
-    }
-    start() {
-        console.log(this);
-        // this refers to the button that has called it 
-    }
+  constructor(startButton, pauseButton, duration) {
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    this.duration = duration;
+    this.startButton.addEventListener('click', this.start);
+  }
+  start() {
+    console.log(this);
+    // this refers to the button that has called it
+  }
 }
 const startButton = document.querySelector('#startButton');
 const pauseButton = document.querySelector('#pauseButton');
 const duration = document.querySelector('#duration');
 
 const timer = new Timer(startButton, pauseButton, duration);
-timer.start() // this line refers to the instance of the class
+timer.start(); // this line refers to the instance of the class
 
-// so how can we make the start() function inside the class 
+// so how can we make the start() function inside the class
 // refers to the same object Timer? and why in the first place ?
-// we can do so either by changing the start function to an arrow function 
+// we can do so either by changing the start function to an arrow function
 // or add bind(this) to the start function call inside the constructor.
 // like that > this.startButton.addEventListener('click', this.start.bind(this));
 // but you should retain your function declaration.
