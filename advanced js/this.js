281cda61f6e3f1fc076f6a33734b0c213d9f3bb3
elghannay <mohamedelghannay@gmail.com>
2020-08-26 21:30:00 +0100

    more on this

--- advanced js/this.js
+++ advanced js/this.js
@@ -26,3 +26,48 @@ a.apply();
 a.call();
 a();
 // three of the calls are equivalent
+
+// call and apply are used to borrow functions from objects
+//  wouldn't be cool to borrow the heal function from the wizard so we can.
+// also heal the archer.
+const wizard = {
+  name: 'merlin',
+  health: 50,
+  heal(points) {
+    return (this.heal = 100 + points);
+  },
+};
+
+const archer = {
+  name: 'merlin',
+  heal: 30,
+};
+console.log('22', archer);
+wizard.heal.apply(archer, [12]);
+wizard.heal.call(archer, 12);
+// the bind keyword it return a function that we must call later.
+const healed = wizard.heal.bind(archer, 25);
+healed();
+console.log('23', archer);
+
+// bind a currying
+function multiplyByTwo(x, y) {
+  return x * y;
+}
+
+const multiply = multiplyByTwo.bind(this, 2);
+// the function expression is  returning us a function
+//with an already passed parameter.
+console.log(multiply(5));
+
+// more examples of the this keyword.
+const c = {
+  name: 'hello',
+  say() {
+    const newF = function () {
+      console.log(this);
+    };
+    return newF.bind(this);
+  },
+};
+c.say()();
